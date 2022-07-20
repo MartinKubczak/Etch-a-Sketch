9ee6071497e7049a20df35a1b93edbd64c570ed0
MartinK <martin.kubczak@gmail.com>
2022-07-21 00:19:46 +0200

    ADD FUNCTION CREATING DIVS

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+    const grid = document.createDocumentFragment(); // document fragment used to store created divs
+
+
+function createDiv() // this function create div elements
+{
+const div = document.createElement('div');
+return div;
+}
+
+
+
