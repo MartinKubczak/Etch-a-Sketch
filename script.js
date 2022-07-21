8211c3339fb5407ed015d11b3a00a6333fdee895
MartinK <martin.kubczak@gmail.com>
2022-07-21 23:34:36 +0200

    ADD HOVER EVENT LISTENER AND CSS CLASS FOR SQUARE DIVS

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const container = document.querySelector(".container");
 const etchSketch = document.querySelector(".etchSketch");
-const newDiv = document.querySelector(".newDiv");
+
+
     const grid = document.createDocumentFragment(); // document fragment used to store created divs
 
 
 function createDiv() // this function create div elements
 {
     const div = document.createElement('div');
-    div.setAttribute("class", "newDiv");
+    div.setAttribute("class", "newDiv");   
+    div.addEventListener("mouseover", () => { div.setAttribute("class", "newDivHover") });
     return div;
 }
+
+
+
 
 for (let i = 0; i < 256; i++) {       //this loop appends particular amount of square divs to grid documentFragment 
     grid.appendChild(createDiv());
